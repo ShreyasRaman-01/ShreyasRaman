0cadb9c73d041288dcf644181e018204b45463b8
Shreyas Raman <shreyas_sundara_raman@brown.edu>
2025-01-03 01:57:55 +0530

    added new media

---
+++ src/components/Blog_Post/Blog.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import CSS from "csstype";
+
+//importing CSS styling for the about card + swipe animation
+import "./Blog.css";
+
+class Blog extends Component {
+
+}
