1faf21065a3da70dcee3b5d343f5a4284cc22803
Shreyas Raman <shreyas_sundara_raman@brown.edu>
2025-01-06 21:17:10 +0530

    smaller debugging

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -47,7 +47,7 @@ const Skills = () => {
               <span className={styles.icon} onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}>{Icon}</span>
             </CircularProgress>
-            <span className={styles.skillName}>{<b>${name}</b>} {<br />} {`${lines} lines`}</span>
+            <span className={styles.skillName}>{<b>{name}</b>} {<br />} {`${lines} lines`}</span>
           </div>
         ))}
       </div>
